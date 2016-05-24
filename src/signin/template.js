5149307d9c1518d3de831158ec7a6731e1108632
Neris <mendozaene@gmail.com>
2016-05-24 12:28:06 -0500

    Agrega iconos como parte del comportamiento de los botones con font-awesome, incluye template para la homepage

--- src/signin/template.js
+++ src/signin/template.js
@@ -8,7 +8,7 @@ var signinForm = yo`<div class="col s12 m7">
 									<h2>Registrate para ver fotos de tus amigos estudiando en Platzi</h2>
 									<div class="section">
 										<a class="btn btn-fb hide-on-small-only">Iniciar sesión con Facebook</a>
-										<a class="btn btn-fb hide-on-med-and-up">Iniciar sesión</a>
+										<a class="btn btn-fb hide-on-med-and-up"><i class="fa fa-facebook-official"></i> Iniciar sesión</a>
 									</div>
 									<div class="divider"></div>
 									<div class="section">
