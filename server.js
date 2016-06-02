33634b678972eb55d22874146af0f17abeaffeb6
Neris <mendozaene@gmail.com>
2016-06-02 01:30:58 -0500

    Permite subir archivos a directorio en servidor a través de Multer

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 var express = require("express");
+var multer = require("multer");
+var ext = require("file-extension");
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, +Date.now() + '.' + ext(file.originalname));
+  }
+})
+ 
+var upload = multer({ storage: storage }).single('picture');
+
 var app = express();
 
 /** Motor de vistas */
@@ -45,6 +59,15 @@ app.get('/api/pictures', function(req, res, next){
 	setTimeout(function(){
 		res.send(pictures);
 	}, 2000);
+});
+
+app.post('/api/pictures', function(req, res){
+	upload(req, res, function(error){
+		if (error) {
+			return res.send(500, "Error!");
+		}
+		res.send("File uploaded!");
+	})
 });
 
 app.listen(8080, function(error){
--- src/homepage/template.js
+++ src/homepage/template.js
@@ -2,12 +2,13 @@ var yo = require('yo-yo');
 var layout = require('../layout');
 var picture = require('../picture-card');
 var translate = require('../translate').message;
+var request = require('superagent');
 
 module.exports = function(pictures){
 	var el = yo`<div class="container timeline">
 		<div class="row">
 			<div class="col s12 m10 offset-m1 l8 offset-l2 center-align">
-				<form enctype="multipart/form-data" class="form-upload" id="formUpload">
+				<form enctype="multipart/form-data" class="form-upload" id="formUpload" onsubmit=${onsubmit}>
 					<div id="fileName" class="fileUpload btn btn-flat cyan">
 						<span><i class="fa fa-camera" aria-hidden="true"></i> ${translate('upload-picture')}</span>
 						<input name="picture" id="file" type="file" class="upload" onchange=${onchange} />
@@ -33,13 +34,24 @@ module.exports = function(pictures){
 	}
 
 	function oncancel(){
-		debugger;
 		toggleButtons();
 		document.getElementById('formUpload').reset();
 	}
 
 	function onchange(){
 		toggleButtons();
+	}
+
+	function onsubmit(evento){
+		evento.preventDefault();
+		
+		var data = new FormData(this);
+		request
+			.post('/api/pictures')
+			.send(data)
+			.end(function(error, res){
+				console.log(arguments);
+			})
 	}
 
 	return layout(el);
