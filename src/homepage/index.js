983b7507e89ccd96210c71383b9079a7f77d8ee5
Neris <mendozaene@gmail.com>
2016-06-01 19:16:23 -0500

    Integra Axios para interactuar con el servidor a través de promesas

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -4,13 +4,15 @@ var template = require('./template');
 var title = require('title');
 var request = require('superagent');
 var header = require('../header');
+var axios = require('axios');
 
-page('/', header, loadPictures, function(context, next){
+page('/', header, loadPicturesAxios, function(context, next){
 	title('Platzigram');
 	var main = document.getElementById('main-container');
 	empty(main).appendChild(template(context.pictures));
 })
 
+/*
 function loadPictures(context, next){
 	request
 		.get('/api/pictures')
@@ -18,5 +20,18 @@ function loadPictures(context, next){
 			if (error) return console.log(error);
 			context.pictures = res.body;
 			next();
+		});
+}
+*/
+
+function loadPicturesAxios(context, next){
+	axios
+		.get('/api/pictures')
+		.then(function(res){
+			context.pictures = res.data;
+			next();
+		})
+		.catch(function(error){
+			console.log(error);
 		});
 }
